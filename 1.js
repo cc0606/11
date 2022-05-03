9066556465f683775fc7c94a782db15f00b69777
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-03 09:43:44 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,38 @@
+
+/**
+ * @fileoverview Template to compose HTTP reqeuest.
+ * 
+ */
+
+const url = `https://api.theone.art/user/api/userCheckIn/save`;
+const method = `POST`;
+const headers = {
+'Accept-Encoding' : `gzip, deflate, br`,
+'Origin' : `https://www.theone.art`,
+'Connection' : `keep-alive`,
+'Accept' : `application/json, text/plain, */*`,
+'Referer' : `https://www.theone.art/`,
+'Host' : `api.theone.art`,
+'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTkxODU1NDMxNSIsImlhdCI6MTY1MTA3Mzk0OSwiZXhwIjoxNjUxNjc4NzQ5fQ.E6U3A_8B5he-2mYBksiFtLLD0ggMEu1Hpto_66PzyvXFlx4XcRszbY80ILlt5HdqdRFBEv9q-G9ZvZ2FF1pfuw
+`,
+'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
+};
+const body = ``;
+
+const myRequest = {
+    url: url,
+    method: method,
+    headers: headers,
+    body: body
+};
+
+$task.fetch(myRequest).then(response => {
+    console.log(response.statusCode + "\n\n" + response.body);
+
+    $notify("1", "Subtitle", response.body); // Success!
+    $done();
+}, reason => {
+    console.log(reason.error);
+    $done();
+});
