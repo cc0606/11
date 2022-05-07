ff8f93cc92ea5b640ae576e4a769a841a1a9e6c2
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-07 09:09:05 +0800

    Update 11.js

--- 11.js
+++ 11.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNzcyMjg1ODcyMCIsImlhdCI6MTY1MTE5MTM0NSwiZXhwIjoxNjUxNzk2MTQ1fQ.r6pDGsvqB1T82phHpRcIcu4j-naxOZmOsobcfwYA2tEvPRz4anhSw_oIinDbYBS500sgeQJ9qXcrxD-tlky-Yg`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNzcyMjg1ODcyMCIsImlhdCI6MTY1MTg0ODcwNCwiZXhwIjoxNjUyNDUzNTA0fQ.KeR_e1yJXfu1DfuZDvkM_VGttIQxpP0q78-UBHHaAkM3LFd38LOp0dWBT3o3L_5G0fc5_vdDHEZesC59FIRJbA`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
