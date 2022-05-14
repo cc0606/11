040271dc057d9a7ab1658910a0ec7b7c1b7e0790
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-15 07:26:38 +0800

    Update 28.js

--- 28.js
+++ 28.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzIxNzY5MzcwMCIsImlhdCI6MTY1MTg0OTk1OSwiZXhwIjoxNjUyNDU0NzU5fQ.kggGFwkde8FoxwWSWNqBP5V5soQi-FSSZxPODTnJvP6IfROvDZy81DHPrZdpe3J0JlsddX9yHGEQA1jsd5QNmA`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTcyNDA3NTgxMyIsImlhdCI6MTY1MjU0MzI0NiwiZXhwIjoxNjUzMTQ4MDQ2fQ.d0OorHF9SGwCRtkYIc0rdgFmOAJktEIBLStCY2zYhA2RcD4lNjjcTIsN_eUzrZBgYAHarOwWzljBm5cgUVYO7w`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
