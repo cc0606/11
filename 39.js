385baff19f059b545eaf2bf0708f1914a56aa4d2
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-15 07:29:44 +0800

    Update 39.js

--- 39.js
+++ 39.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTcxMTgyMzAxNiIsImlhdCI6MTY1MTg1MTIwMCwiZXhwIjoxNjUyNDU2MDAwfQ.BJdxNiF0gef2oNSsbZBIDIEaAp3LQ_uM_VJS5t5h5IY_93-Kl4suHKW22eHU6JTuh7WPmPQPRu7rWwau0rP1cw`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTcxMTc4OTY4NSIsImlhdCI6MTY1MjU0Mzg1MSwiZXhwIjoxNjUzMTQ4NjUxfQ.wTJ1uw85-ObgESTQYRw_KGtnNlb5IuDCmkrUBBaJuPhZcMG6SoVL-YFUEEfthPd6r-7MSgutWehN61N4KcUbPA`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
