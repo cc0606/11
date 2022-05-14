5e9f74c79f42a645efca7d5460a9c35122a02b3c
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-15 07:24:42 +0800

    Update 21.js

--- 21.js
+++ 21.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTAxMzAzNzIzNiIsImlhdCI6MTY1MTg0OTUwOSwiZXhwIjoxNjUyNDU0MzA5fQ.IWHoZ1I6rr8VzeDRBtKVkrQaQfoI5081kNg1pMP1qUchaiq36MMpCFdzcSW-1zO5xKWhiRPgotDIAxLqA5sQYw`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNzM0ODczNzU5NCIsImlhdCI6MTY1MjU0MjcwOSwiZXhwIjoxNjUzMTQ3NTA5fQ.-lVOaaAJirGCq7SUrLAaHzqlEpm2Rm3bFF_-gOW9OKacC6ErsyDWoSezfOIo41QF32I6ONMxsgbpHMKO0WrmkA`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
