076fba3274c4e37dd773286612ce56a4dbb6e026
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-15 07:27:44 +0800

    Update 32.js

--- 32.js
+++ 32.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTcyODQxODE5MSIsImlhdCI6MTY1MTg1MDMxMSwiZXhwIjoxNjUyNDU1MTExfQ.jD9fVQMLNmjyctrxWM6sbbfn_Q-ZT1Lyw8NjM4YviTopwj-aWrrK28dIuJOq0BqP1HzFbfAryr8v1yYMM-3K8A`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTcyODQxODE5MSIsImlhdCI6MTY1MjU0MzU5OCwiZXhwIjoxNjUzMTQ4Mzk4fQ.T5XHFCwkwKmJhL8SumOjPwvqvaD2KX68UhHZkYrPJkgabxrE5wMha4zR1PlkOXHkYfIQ5YPgN9KZzS0PyfJr7Q`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
