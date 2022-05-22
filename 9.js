4a653cfbfd8c47d667544ac289f0be1f02834c19
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-22 16:55:02 +0800

    Update 9.js

--- 9.js
+++ 9.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNzcyMjg2MDkzNSIsImlhdCI6MTY1MjU0MTk2MCwiZXhwIjoxNjUzMTQ2NzYwfQ.DU57oBbwRcCRSYm_fhVb4LX_WeRBbjA5I3tQCU085D_lKQkqOGYjmY-ioQXuuT6pv3q9T-zp-6vKFmoMkZuEeQ`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNzcyMjg2MDkzNSIsImlhdCI6MTY1MzIwMTk5MCwiZXhwIjoxNjUzODA2NzkwfQ.vxop-aHyPkuLBmXZPHANwoiEke1rqnC8uSNF_9Q5zpY5vfm8hpyoDtEHskPJ3AwV-o1AJsIeVOps0PFS9LJDHQ`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
