f8f86282b392bd60020ee322767bb180f4f97314
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-22 17:03:34 +0800

    Update 40.js

--- 40.js
+++ 40.js
@@ -14,7 +14,7 @@ const headers = {
 'Referer' : `https://www.theone.art/`,
 'Host' : `api.theone.art`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1`,
-'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTcxMTgyMzAxNiIsImlhdCI6MTY1MjU0MzkzMCwiZXhwIjoxNjUzMTQ4NzMwfQ.UjpKNyQhGJgLBuUJWMb1xf4_4F8OyODQn3rv-XV3Gmv5cxx4jXYUfCh3-UUoMvOTRpKxS32KDohZlR7IwZdtkA`,
+'Authorization' : `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTcxMTgyMzAxNiIsImlhdCI6MTY1MzIwMzk2NCwiZXhwIjoxNjUzODA4NzY0fQ.HVjQg7I3CFSVNuhBm2VWMHi82p1EpEi304Xh4ulXpciYb4Xy1rTHrP-YX1HXNATwl6L2e10Wte02wzx0qyX4KQ`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
 const body = ``;
